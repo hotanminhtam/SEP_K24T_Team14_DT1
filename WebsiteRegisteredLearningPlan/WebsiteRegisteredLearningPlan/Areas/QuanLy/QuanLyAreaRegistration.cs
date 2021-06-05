ceba4ea25a146776fb5fb2c32f949b1e3974d938
Thanhnhanss <55831670+Thanhnhanss@users.noreply.github.com>
2021-06-05 16:39:20 +0700

    Add template Sinh Vien, chinh sua, chua hoan chinh

---
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/QuanLy/QuanLyAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebsiteRegisteredLearningPlan.Areas.QuanLy
+{
+    public class QuanLyAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "QuanLy";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "QuanLy_default",
+                "QuanLy/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
