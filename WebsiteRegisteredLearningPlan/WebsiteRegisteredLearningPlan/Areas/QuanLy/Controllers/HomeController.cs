e462dca2067304799b49c6dec65ac95734a8b2c8
Thanhnhanss <55831670+Thanhnhanss@users.noreply.github.com>
2021-06-06 15:04:38 +0700

    Cap nhat template QuanLy, Chuc nang Dang nhap (QL,SV)

--- WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/QuanLy/Controllers/HomeController.cs
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/QuanLy/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace WebsiteRegisteredLearningPlan.Areas.QuanLy.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         // GET: QuanLy/Home
