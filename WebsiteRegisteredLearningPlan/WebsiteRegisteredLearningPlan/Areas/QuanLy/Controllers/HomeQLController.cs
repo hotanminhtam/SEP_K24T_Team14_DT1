33e452de18d14657b029274acaeaa2f9776ce40c
Hồ Tấn Minh Tâm <hotanminhtam1703@gmail.com>
2021-06-11 02:27:02 +0700

    Add template QuanLy + SinhVien Function Login

---
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/QuanLy/Controllers/HomeQLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteRegisteredLearningPlan.Areas.QuanLy.Controllers
+{
+    public class HomeQLController : Controller
+    {
+        // GET: QuanLy/HomeQL
+        public ActionResult HomeQL()
+        {
+            return View();
+        }
+    }
+}
