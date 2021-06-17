8a740cc14a6bcdc80bfd90c7f86a188e36c4323e
Thanhnhanss <55831670+Thanhnhanss@users.noreply.github.com>
2021-06-17 19:41:16 +0700

    fix template

---
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/SinhVien/Controllers/DangKyKHHTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteRegisteredLearningPlan.Areas.SinhVien.Controllers
+{
+    public class DangKyKHHTController : Controller
+    {
+        // GET: SinhVien/DangKyKHHT
+        public ActionResult DangKyKHHT()
+        {
+            return View();
+        }
+    }
+}
