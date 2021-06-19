417ae958c38b22ace17f91ef612ff3c0c662b77b
Thanhnhanss <55831670+Thanhnhanss@users.noreply.github.com>
2021-06-19 22:31:34 +0700

    Dang ky KHHT  (doing)

--- WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/SinhVien/Controllers/DangKyKHHTController.cs
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/SinhVien/Controllers/DangKyKHHTController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteRegisteredLearningPlan.Models;
 
 namespace WebsiteRegisteredLearningPlan.Areas.SinhVien.Controllers
 {
     public class DangKyKHHTController : Controller
     {
+        Entities db = new Entities();
         // GET: SinhVien/DangKyKHHT
         public ActionResult DangKyKHHT()
         {
-            return View();
+            // ben nay de kieu gi, ben kia de y nhu v
+            // ow day no de List<CTDT>
+            var ngayHienTai = DateTime.Now;
+            var hocKyHienTai = db.HOCKies.ToList().First(item => check(item.ngaykt.Value, item.ngaybd.Value)).mahk;
+            var chuongTrinhDaoTao = db.CTDTs.Where(item => item.hocky == hocKyHienTai).ToList();
+            ViewBag.hocKyHienTai = hocKyHienTai;
+            return View(chuongTrinhDaoTao);
+        }
+
+        private bool check(DateTime ngayKt, DateTime ngayBd)
+        {
+            return ngayKt - ngayBd >= DateTime.Now - ngayBd;
         }
     }
 }
