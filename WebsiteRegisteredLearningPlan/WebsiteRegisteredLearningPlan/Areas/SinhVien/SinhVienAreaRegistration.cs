d08324f8da18650b08cae27671c827271da7cda4
khanh duy <kfjdkfj@gmail.com>
2021-06-21 17:15:47 +0700

    add template

---
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/SinhVien/SinhVienAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebsiteRegisteredLearningPlan.Areas.SinhVien
+{
+    public class SinhVienAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SinhVien";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SinhVien_default",
+                "SinhVien/{controller}/{action}/{id}",
+                new { action = "HomeSV", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
