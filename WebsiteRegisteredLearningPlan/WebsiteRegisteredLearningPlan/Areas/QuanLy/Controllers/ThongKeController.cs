af5b86450fc5560f0b7962f58495a1511d08dac1
hungdp2612 <55739558+hungdp2612@users.noreply.github.com>
2021-07-16 22:18:35 +0700

    Thống kê

--- WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/QuanLy/Controllers/ThongKeController.cs
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Areas/QuanLy/Controllers/ThongKeController.cs
@@ -45,118 +45,13 @@ namespace WebsiteRegisteredLearningPlan.Areas.QuanLy.Controllers
             Response.AddHeader("content-disposition", "attachment; filename=" + "ThongKe.xlsx");
             Response.BinaryWrite(ep.GetAsByteArray());
             Response.End();
-            
         }
-        // GET: QuanLy/ThongKe/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            KETQUADANGKY kETQUADANGKY = db.KETQUADANGKies.Find(id);
-            if (kETQUADANGKY == null)
-            {
-                return HttpNotFound();
-            }
-            return View(kETQUADANGKY);
-        }
-
-        // GET: QuanLy/ThongKe/Create
-        public ActionResult Create()
-        {
-            ViewBag.email = new SelectList(db.AspNetUsers, "Id", "Email");
-            ViewBag.mahp = new SelectList(db.CTDTs, "id", "tenhp");
-            return View();
-        }
-
-        // POST: QuanLy/ThongKe/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "email,mahp,ngaydk,id")] KETQUADANGKY kETQUADANGKY)
+        public JsonResult ChartData()
         {
-            if (ModelState.IsValid)
-            {
-                db.KETQUADANGKies.Add(kETQUADANGKY);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.email = new SelectList(db.AspNetUsers, "Id", "Email", kETQUADANGKY.email);
-            ViewBag.mahp = new SelectList(db.CTDTs, "id", "tenhp", kETQUADANGKY.mahp);
-            return View(kETQUADANGKY);
-        }
-
-        // GET: QuanLy/ThongKe/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            KETQUADANGKY kETQUADANGKY = db.KETQUADANGKies.Find(id);
-            if (kETQUADANGKY == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.email = new SelectList(db.AspNetUsers, "Id", "Email", kETQUADANGKY.email);
-            ViewBag.mahp = new SelectList(db.CTDTs, "id", "tenhp", kETQUADANGKY.mahp);
-            return View(kETQUADANGKY);
-        }
-
-        // POST: QuanLy/ThongKe/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "email,mahp,ngaydk,id")] KETQUADANGKY kETQUADANGKY)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(kETQUADANGKY).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.email = new SelectList(db.AspNetUsers, "Id", "Email", kETQUADANGKY.email);
-            ViewBag.mahp = new SelectList(db.CTDTs, "id", "tenhp", kETQUADANGKY.mahp);
-            return View(kETQUADANGKY);
-        }
-
-        // GET: QuanLy/ThongKe/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            KETQUADANGKY kETQUADANGKY = db.KETQUADANGKies.Find(id);
-            if (kETQUADANGKY == null)
-            {
-                return HttpNotFound();
-            }
-            return View(kETQUADANGKY);
-        }
-
-        // POST: QuanLy/ThongKe/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            KETQUADANGKY kETQUADANGKY = db.KETQUADANGKies.Find(id);
-            db.KETQUADANGKies.Remove(kETQUADANGKY);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            var noidung = db.KETQUADANGKies.GroupBy(item => item.CTDT.tenhp)
+                .Select(item2 => new { name = item2.Key, count = item2.Count() }).ToList();
+            return Json(new { dbchart = noidung }, JsonRequestBehavior.AllowGet);
         }
     }
 }
