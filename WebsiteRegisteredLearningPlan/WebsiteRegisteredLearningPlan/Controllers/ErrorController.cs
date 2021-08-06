b5224a03f808753ec5f5fc41b82c343e5e0001cd
Thanhnhanss <55831670+Thanhnhanss@users.noreply.github.com>
2021-08-06 19:22:43 +0700

    Error func

---
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteRegisteredLearningPlan.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Regular Error";
+            return View();
+        }
+
+        public ActionResult NotFound404()
+        {
+            ViewBag.Title = "Error 404 - File not Found";
+            return View("Index");
+        }
+    }
+}
--- WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Global.asax.cs
+++ WebsiteRegisteredLearningPlan/WebsiteRegisteredLearningPlan/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebsiteRegisteredLearningPlan.Controllers;
 
 namespace WebsiteRegisteredLearningPlan
 {
@@ -17,5 +18,33 @@ namespace WebsiteRegisteredLearningPlan
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Do whatever you want to do with the error
+
+            //Show the custom error page...
+            Server.ClearError();
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+
+            if ((Context.Server.GetLastError() is HttpException) && ((Context.Server.GetLastError() as HttpException).GetHttpCode() != 404))
+            {
+                routeData.Values["action"] = "Index";
+            }
+            else
+            {
+                // Handle 404 error and response code
+                Response.StatusCode = 404;
+                routeData.Values["action"] = "NotFound404";
+            }
+            Response.TrySkipIisCustomErrors = true; // If you are using IIS7, have this line
+            IController errorsController = new ErrorController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new System.Web.Routing.RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
+
+            Response.End();
+        }
     }
+
 }
